1b04c1c9951de99be08afd7db3c1a1cfb52b215f
Matheus Souza <matheu.sk.9@hotmail.com>
2019-08-08 17:34:47 -0300

    Load fonts in App component

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import * as Font from 'expo-font';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      fontLoaded: false
+    }
+  }
+  
+  async componentDidMount() {
+    await Font.loadAsync({
+      'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf')
+    })
+
+    this.setState({
+      fontLoaded: true
+    })
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.fontLoaded && <Text style={{fontFamily: "OpenSans-Bold"}}>Fonts have been loaded successfully</Text>}
+        {!this.state.fontLoaded && <Text>No fonts have been loaded</Text>}
+      </View>
+    );
+  }
+}
