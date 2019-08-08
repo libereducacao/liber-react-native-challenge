35390e6932577cb408ce3e45c11385bacc156df1
Matheus Souza <matheu.sk.9@hotmail.com>
2019-08-08 18:43:35 -0300

    Add gradient backgroundcolor

--- App.js
+++ App.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingVertical: 34,
+    paddingHorizontal: 36
   }
 });
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       fontLoaded: false
-    }
+    };
   }
-  
+
   async componentDidMount() {
     await Font.loadAsync({
       'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
       'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf')
-    })
+    });
 
     this.setState({
       fontLoaded: true
-    })
+    });
   }
-  
+
   render() {
     return (
-      <View style={styles.container}>
-        {this.state.fontLoaded && <Text style={{fontFamily: "OpenSans-Bold"}}>Fonts have been loaded successfully</Text>}
+      <LinearGradient
+        colors={['#F6F7F9', '#F6F7F9', '#F6F7F9']}
+        style={styles.container}
+      >
+        {this.state.fontLoaded && (
+          <Text style={{ fontFamily: 'OpenSans-Bold' }}>
+            Fonts have been loaded successfully
+          </Text>
+        )}
         {!this.state.fontLoaded && <Text>No fonts have been loaded</Text>}
-      </View>
+      </LinearGradient>
     );
   }
 }
